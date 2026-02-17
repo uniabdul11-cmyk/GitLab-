447147cb35b279a6e5f5fa0719adc0265d627fac
abod <uniabdul11@gmail.com>
2026-02-17 08:41:48 +0300

    intial commit: Add HellwWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+public static void main(String[] args) {
+System.out.println("Hello, SE4111!");
+System.out.println("updates new Welcome to Software Construction");
+}
+} 
